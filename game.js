b7c7af1027cb1d6c0bcea62ebfea6510da102d4c
imyooni <120503455+imyooni@users.noreply.github.com>
2025-02-27 21:23:34 -0300

    Update game.js

--- game.js
+++ game.js
@@ -40,7 +40,7 @@ document.addEventListener('contextmenu', (event) => {
 // board //
 ///////////
 let baseBoardPos = [100,250]
-let pieceSize = 42
+let pieceSize = 32
 let cols = 5;
 let rows = 10;
 let pieces = new Array(cols*rows).fill(null);
